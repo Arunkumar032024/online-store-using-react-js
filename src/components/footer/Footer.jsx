dfe8d71643a9f301e5397cd32382a6b73d4d03cc
Arunkumar032024 <arunkumar032024@gmail.com>
2024-08-05 14:51:48 +0530

    fix footer bug

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='text-center p-5 border-t-[1px] border-t-rose-200'>
-        <p>Copyright &copy; 2024 <a href="https://developer-ak-032024.vercel.app" target='_blank' className='hover:text-rose-800 underline'>Developer Ak</a>, Designed by <a href="mailto:arunkumar03204@gmail.com" className='hover:text-rose-800 underline'>Arun kumar</a></p>
+        <p>Copyright &copy; 2024 <a rel="noreferrer" href="https://developer-ak-032024.vercel.app" target='_blank' className='hover:text-rose-800 underline'>Developer Ak</a>, Designed by <a href="mailto:arunkumar03204@gmail.com" className='hover:text-rose-800 underline'>Arun kumar</a></p>
     </footer>
   )
 }
